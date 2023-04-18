c987eabd1e10b7683467d3726fdce03ce10945cf
Alexander Hung <hungalex@hawaii.edu>
2023-04-17 19:52:27 -1000

    update DateField

--- app/imports/ui/pages/CreateStudySession.jsx
+++ app/imports/ui/pages/CreateStudySession.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, Col, Container, Row } from 'react-bootstrap';
-import { AutoForm, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
+import { AutoForm, DateField, ErrorsField, LongTextField, SelectField, SubmitField, TextField } from 'uniforms-bootstrap5';
 import swal from 'sweetalert';
 import { Meteor } from 'meteor/meteor';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
@@ -56,7 +56,7 @@ const CreateStudySession = () => {
             <Card>
               <Card.Body>
                 <TextField name="name" />
-                <TextField name="date" />
+                <DateField name="date" showInlineError type="datetime-local" />
                 <SelectField name="icsclass" />
                 <LongTextField name="description" />
                 <SubmitField value="Submit" />
