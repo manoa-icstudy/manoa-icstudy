3dce5f55679b3240caad668ea765a928f40f34ce
Alexander Hung <hungalex@hawaii.edu>
2023-04-20 00:34:46 -1000

    fix lint

--- app/imports/ui/pages/SignIn.jsx
+++ app/imports/ui/pages/SignIn.jsx
@@ -5,7 +5,6 @@ import { Alert, Card, Col, Container, Row } from 'react-bootstrap';
 import SimpleSchema from 'simpl-schema';
 import SimpleSchema2Bridge from 'uniforms-bridge-simple-schema-2';
 import { AutoForm, ErrorsField, SubmitField, TextField } from 'uniforms-bootstrap5';
-import swal from 'sweetalert';
 import { LoginLog } from '../../api/log/LoginLog';
 
 /**
