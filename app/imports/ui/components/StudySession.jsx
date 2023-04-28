1a054cc8b5eff837c06ccf593875900e1a4ac344
Alexander Hung <hungalex@hawaii.edu>
2023-04-28 12:04:10 -1000

    fix layout

--- app/imports/ui/components/StudySession.jsx
+++ app/imports/ui/components/StudySession.jsx
@@ -1,53 +1,89 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useTracker } from 'meteor/react-meteor-data';
 import { Meteor } from 'meteor/meteor';
 import { Roles } from 'meteor/alanning:roles';
-import { Button, Card, CardGroup, Container, ListGroup, Modal } from 'react-bootstrap';
+import { Button, Card, Col, ListGroup, Modal, Row } from 'react-bootstrap';
 import { Calendar2, Trash, PeopleFill } from 'react-bootstrap-icons';
-
+import swal from 'sweetalert';
+import { Sessions } from '../../api/session/Session';
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
 const StudySession = ({ session, collection }) => {
+  const { currentUser } = useTracker(() => ({
+    currentUser: Meteor.user() ? Meteor.user().username : '',
+  }), []);
   const removeItem = (docID) => {
     collection.remove(docID);
   };
+
+  const join = (doc) => {
+    if (!(doc.participant.find(user => user === currentUser))) {
+      console.log(Intl.DateTimeFormat().resolvedOptions().timeZone);
+      doc.participant.push(currentUser);
+      const newParticipant = doc.participant;
+      Sessions.collection.update(doc._id, { $set: { participant: newParticipant } });
+      swal('Success', 'Join success', 'success');
+    } else if ((doc.participant.find(user => user === currentUser)) && (currentUser !== doc.owner)) {
+      const index = doc.participant.indexOf(currentUser);
+      if (index > -1) {
+        doc.participant.splice(index, 1);
+      }
+      const newParticipant = doc.participant;
+      Sessions.collection.update(doc._id, { $set: { participant: newParticipant } });
+      swal('Success', 'Quit success', 'success');
+    } else {
+      swal('Error', 'You own this Session', 'error');
+    }
+  };
+
+  const options = {
+    hour: 'numeric',
+    minute: 'numeric',
+    weekday: 'short',
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'Pacific/Honolulu',
+
+  };
+
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   return (
-    <Container>
-      <CardGroup>
-        <Card>
-          <Card.Body>
-            <Card.Title>{session.icsclass}</Card.Title>
-            <ListGroup>
-              <ListGroup.Item><PeopleFill /><Card.Text className="d-inline-block">{session.name}</Card.Text></ListGroup.Item>
-              <ListGroup.Item><Calendar2 /><Card.Text className="d-inline-block">{session.date.toDateString()}</Card.Text></ListGroup.Item>
-            </ListGroup>
-          </Card.Body>
-          <Card.Footer>
-            <Button id="left-panel-link" href="/create-report" variant="warning" style={{ color: 'black' }}>Report it</Button>
-            {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
-              <td><Button id="left-panel-link" onClick={() => removeItem(session._id)}><Trash /></Button></td>
-            ) : ''}
-            <>
-              <Button id="right-panel-link" onClick={handleShow}>Learn More</Button>
-              <Modal show={show} onHide={handleClose} animation={false}>
-                <Modal.Header closeButton>
-                  <Modal.Title>{session.icsclass}</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>{session.description}</Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            </>
-          </Card.Footer>
-        </Card>
-      </CardGroup>
-    </Container>
+    <Card style={{ width: '25rem' }} className="p-0 m-2">
+      <Card.Header>
+        <Row>
+          <Col><Card.Title><h5 className="m-2">{session.icsclass}</h5></Card.Title></Col>
+          <Col className="text-end"><Button id="left-panel-link" href="/create-report" variant="warning" style={{ color: 'black' }}>Report</Button></Col>
+        </Row>
+      </Card.Header>
+      <Card.Body>
+        <ListGroup>
+          <ListGroup.Item><PeopleFill /><Card.Text className="d-inline-block">{session.name}</Card.Text></ListGroup.Item>
+          <ListGroup.Item><Calendar2 /><Card.Text className="d-inline-block">{new Intl.DateTimeFormat('en-US', options).format(session.date)}</Card.Text></ListGroup.Item>
+        </ListGroup>
+      </Card.Body>
+      <Card.Footer className="text-end">
+        <Button id="right-panel-link" onClick={handleShow}>Learn More</Button>
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>{session.icsclass}</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>{session.description}</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+        {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
+          <Button id="left-panel-link" variant="danger" onClick={() => removeItem(session._id)}><Trash /></Button>
+        ) : ''}
+
+      </Card.Footer>
+    </Card>
   );
 };
 
