5e857d0c4a24f8923a2d7d8a850103a8df852762
jmnakano <jmnakano@hawaii.edu>
2023-04-30 11:34:24 -1000

    Redirect to landing page after sign out

--- app/imports/ui/pages/SignOut.jsx
+++ app/imports/ui/pages/SignOut.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
-import { Col } from 'react-bootstrap';
+import { Navigate } from 'react-router-dom';
+import swal from 'sweetalert';
 
 /* After the user clicks the "SignOut" link in the NavBar, log them out and display this page. */
 const SignOut = () => {
   Meteor.logout();
+  swal('You are signed out');
+
+  const { from } = { from: { pathname: '/home' } };
+
   return (
-    <Col id="signout-page" className="text-center py-3"><h2>You are signed out.</h2></Col>
+    <Navigate to={from} />
   );
 };
 
